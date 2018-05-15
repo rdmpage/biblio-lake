ce3957f7d59efb15d2d205dc17d0f4a2899bca91
Roderic Page <rdmpage@gmail.com>
2018-05-15 06:35:13 +0100

    Add code to dequeue additional links t

--- actions/dequeue.php
+++ actions/dequeue.php
@@ -4,10 +4,31 @@
 
 require_once(dirname(dirname(__FILE__)) . '/queue/queue.php');
 
+// Array to hold links we discover
+$links = array();
+
 while (!queue_is_empty())
-{
-	dequeue(100);
+{	
+	$result = dequeue(100);
+	
+	$links = array_merge($links, $result);
 }
 
+// links we have found
+print_r($links);
+
+// For each additional link add it to the queue if we haven't already found it
+
+foreach ($links as $url)
+{
+	echo $url . "\n";
+	enqueue($url, false);
+}
+
+// Empty queue, ignoring any further links
+while (!queue_is_empty())
+{	
+	dequeue(100);
+}
 
 ?>
